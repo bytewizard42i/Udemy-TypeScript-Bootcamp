2ee9eb4980cf3365188bbb5c4826e2b8961b3a8e
bytewizard42i <100811071+bytewizard42i@users.noreply.github.com>
2024-08-17 04:11:03 -0400

    change from js to ts file

---
+++ 01-why-typescript.ts
@@ -0,0 +1,42 @@
+
+//lesson 01 
+
+// console.log("*** Beginning of Program ***");
+
+// console.log(100);
+
+// console.log([0, 1, 2]);
+
+// const name = "Typescript Bootcamp";
+
+// printCourseName(name);
+
+// function printCourseName(name) {
+
+//     console.log("The course name is " + name.toUpperCase());
+
+// };
+
+// console.log("*** End of Program ***");
+
+
+
+
+console.log("*** Beginning of Program ***");
+
+console.log(100);
+
+console.log([0, 1, 2]);
+
+const courseName = "Typescript Bootcamp";
+
+printCourseName(courseName);
+
+// function printCourseName(name) {
+function printCourseName(courseName) {
+
+    console.log("The course name is: \n" + courseName.toUpperCase());
+
+};
+
+console.log("*** End of Program ***");
